a30f80a02c792400a6ee3fd9b88a62cb63ad2459
Galen Palmer <palmergs@gmail.com>
2015-02-27 20:14:29 -0500

    add logged limited page to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :about ]
+
   def index
   end
 
   def about
+  end
+
+  def profile
   end
 end
