990f8bc5452110000e0d03dc9c9972f07bda123a
Galen Palmer <palmergs@gmail.com>
2015-12-28 21:26:45 -0500

    structure associations

--- app/models/structure_item.rb
+++ app/models/structure_item.rb
@@ -1,2 +1,8 @@
 class StructureItem < ApplicationRecord
+
+  has_many :content_items
+
+  has_many :parent_relations, class_name: 'StructureRelation', foreign_key: :parent_id, primary_key: :id
+  has_many :child_relations, class_name: 'StructureRelation', foreign_key: :child_id, primary_key: :id
+
 end
--- app/models/structure_relation.rb
+++ app/models/structure_relation.rb
@@ -1,2 +1,6 @@
 class StructureRelation < ApplicationRecord
+
+  belongs_to :parent, class_name: 'StructureItem', foreign_key: :parent_id, primary_key: :id
+  belongs_to :child, class_name: 'StructureItem', foreign_key: :child_id, primary_key: :id
+
 end
