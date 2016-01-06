7c9fa7c18fc16418f0c83364ab368e7cb2fa864b
Galen Palmer <palmergs@gmail.com>
2016-01-05 21:55:08 -0500

    allow for null parents

--- db/migrate/20151229013851_create_content_items.rb
+++ db/migrate/20151229013851_create_content_items.rb
@@ -1,8 +1,8 @@
 class CreateContentItems < ActiveRecord::Migration[5.0]
   def change
     create_table :content_items do |t|
+      t.integer :parent_id,         index: true
       t.integer :structure_item_id, null: false, index: true
-      t.integer :parent_id,         null: false, index: true      
       t.json :params,               null: false, default: {}
       t.text :content,              null: false, default: ''
       t.integer :rel_order,         null: false, default: 0, index: true
