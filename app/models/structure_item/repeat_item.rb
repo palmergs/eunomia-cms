7b7027c3d627a933ad9daab6eda0f5edcb3ea747
Galen Palmer <palmergs@gmail.com>
2017-08-20 15:42:53 -0400

    adding base repeat item

---
+++ app/models/structure_item/repeat_item.rb
@@ -0,0 +1,2 @@
+class StructureItem::RepeatItem < StructureItem
+end
---
+++ test/models/structure_item/repeat_item_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StructureItem::RepeatItemTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
