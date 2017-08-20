b5a4068d99d697e5457a8a5d0d980110e69ae8d5
Galen Palmer <palmergs@gmail.com>
2017-08-20 12:35:07 -0400

    make association to parent optional

--- app/models/content_item.rb
+++ app/models/content_item.rb
@@ -2,7 +2,7 @@ class ContentItem < ApplicationRecord
 
   belongs_to :structure_item
 
-  belongs_to :parent, class_name: 'ContentItem', foreign_key: :parent_id, primary_key: :id
+  belongs_to :parent, class_name: 'ContentItem', foreign_key: :parent_id, primary_key: :id, optional: true
   has_many :children, class_name: 'ContentItem', foreign_key: :parent_id, primary_key: :id
 
 end
