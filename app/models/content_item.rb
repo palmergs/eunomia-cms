94750ac519b38d5aee524bff2b4d2c574f727ef0
Galen Palmer <palmergs@gmail.com>
2017-08-27 17:45:22 -0400

    formatting

--- app/models/content_item.rb
+++ app/models/content_item.rb
@@ -6,11 +6,15 @@ class ContentItem < ApplicationRecord
 
   belongs_to :structure_item
 
-  belongs_to :parent, class_name: 'ContentItem', foreign_key: :parent_id, primary_key: :id, optional: true
-  has_many :children, class_name: 'ContentItem', foreign_key: :parent_id, primary_key: :id
+  belongs_to :parent, 
+      class_name: 'ContentItem', 
+      foreign_key: :parent_id,
+      primary_key: :id, 
+      optional: true
 
-  def valid_children?
-
-  end
+  has_many :children, 
+      class_name: 'ContentItem', 
+      foreign_key: :parent_id, 
+      primary_key: :id
 
 end
