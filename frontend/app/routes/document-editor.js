949bef26c9466efcfcd082f8154d60e59dffdb4d
Galen Palmer <palmergs@gmail.com>
2017-08-27 17:24:52 -0400

    about to move keypress detection into content editor

--- frontend/app/routes/document-editor.js
+++ frontend/app/routes/document-editor.js
@@ -6,6 +6,7 @@ export default Ember.Route.extend({
     let doc = store.createRecord('content-document');
     let block = store.createRecord('content-block');
     block.set('contentDocument', doc);
+    block.set('text', "This is a pre-built content block");
     return doc;
   }
 });
